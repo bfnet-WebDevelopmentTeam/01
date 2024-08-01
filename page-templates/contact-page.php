21899bf600c9649b2e4dfba4880348ad8ae91833
BFネット/Web開発チーム <163156107+bfnet-WebDevelopmentTeam@users.noreply.github.com>
2024-08-01 16:11:49 +0900

    Add files via upload
    Upload file from browse

--- page-templates/contact-page.php
+++ page-templates/contact-page.php
@@ -12,6 +12,22 @@ get_header(); ?>
 <article id="main_contents">
 </article>
 
+<!--お知らせ▼-->
+<style type="text/css">
+.tg  {border-collapse:collapse;border-color:#9ABAD9;border-spacing:0;}
+.tg td{background-color:#EBF5FF;border-color:#9ABAD9;border-style:solid;border-width:1px;color:#444;
+  font-family:Arial, sans-serif;font-size:14px;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg th{background-color:#409cff;border-color:#9ABAD9;border-style:solid;border-width:1px;color:#fff;
+  font-family:Arial, sans-serif;font-size:14px;font-weight:normal;overflow:hidden;padding:10px 5px;word-break:normal;}
+.tg .tg-6uft{border-color:#cbcefb;font-size:16px;text-align:left;vertical-align:top}
+</style>
+<table width="100%" class="tg"><thead>
+  <tr>
+    <td>夏季休暇のお知らせ<br><br>平素は格別のご愛顧くださり心より御礼申し上げます。<br>誠に勝手ながら、以下の期間を休業とさせていただきます。<br><br>夏季休暇<br>令和6年8月14日（水）～ 令和6年8月18日（日）<br><br>※休業期間中にお問い合わせいただきました件に関しては、<br>令和6年5月19日より順次ご対応させていただきます。<br>ご迷惑をお掛けいたしますが、何卒ご了承くださいますよう宜しくお願い申し上げます。<br></td>
+  </tr></thead>
+</table><br><br><br>
+<!--お知らせ▲-->
+
 <section id="contact">
   <div id="formWrap">
     <div id="recruit_box2">
