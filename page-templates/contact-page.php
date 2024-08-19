d94aabebad2c90838cecdfda2032997ec1f4b782
bfnet-WebDevelopmentTeam <bfnet.0911@gmail.com>
2024-08-19 11:37:59 +0900

    page-templates/contact-page.php commit

--- page-templates/contact-page.php
+++ page-templates/contact-page.php
@@ -12,7 +12,7 @@ get_header(); ?>
 <article id="main_contents">
 </article>
 
-<!--お知らせ▼-->
+<!--お知らせ▼
 <style type="text/css">
 .tg  {border-collapse:collapse;border-color:#9ABAD9;border-spacing:0;}
 .tg td{background-color:#EBF5FF;border-color:#9ABAD9;border-style:solid;border-width:1px;color:#444;
@@ -26,7 +26,7 @@ get_header(); ?>
     <td>夏季休暇のお知らせ<br><br>平素は格別のご愛顧くださり心より御礼申し上げます。<br>誠に勝手ながら、以下の期間を休業とさせていただきます。<br><br>夏季休暇<br>令和6年8月14日（水）～ 令和6年8月18日（日）<br><br>※休業期間中にお問い合わせいただきました件に関しては、<br>令和6年5月19日より順次ご対応させていただきます。<br>ご迷惑をお掛けいたしますが、何卒ご了承くださいますよう宜しくお願い申し上げます。<br></td>
   </tr></thead>
 </table><br><br><br>
-<!--お知らせ▲-->
+お知らせ▲-->
 
 <section id="contact">
   <div id="formWrap">
